b2a1bafd9352c8b03dffaef8d69d3a58d088f489
Ethan Scime <escimeaqua@gmail.com>
2023-10-27 16:37:13 -0400

    Merged with the single controller branch to bring along everything I fixed in there. Hopefully re-implemented everything for the operator controller correctly. Will need to be tested.

--- robotcontainer.py
+++ robotcontainer.py
@@ -40,6 +40,7 @@ class RobotContainer:
 
         # Setup driver & operator controllers.
         self.driver_controller_raw = CustomHID(OIConstants.kDriverControllerPort, "xbox")
+        self.operator_controller_raw = CustomHID(OIConstants.kOperatorControllerPort, "xbox")
         DriverStation.silenceJoystickConnectionWarning(True)
 
         # Set the default drive command.
@@ -72,18 +73,14 @@ class RobotContainer:
         SmartDashboard.putData("Debug Mode On", DebugMode(self.robot_drive, True))
         SmartDashboard.putData("Debug Mode Off", DebugMode(self.robot_drive, False))
 
-        # Set default arm command to auto setpointing.
-        self.arm.setDefaultCommand(commands2.cmd.run(lambda: self.arm.auto_setpoint(self.robot_drive),
-                                                     [self.arm]))
-
     def configureTriggers(self) -> None:
         """Used to set up any commands that trigger when a measured event occurs."""
         # Parking Brake.
-        commands2.Trigger(lambda: self.driver_controller_raw.get_button("LB")).whileTrue(
+        commands2.Trigger(lambda: self.driver_controller_raw.get_trigger("L", 0.05)).whileTrue(
             commands2.cmd.run(lambda: self.robot_drive.drive_lock(), [self.robot_drive]))
 
         # Slow mode.
-        commands2.Trigger(lambda: self.driver_controller_raw.get_button("RB")).whileTrue(
+        commands2.Trigger(lambda: self.driver_controller_raw.get_trigger("R", 0.05)).whileTrue(
             commands2.cmd.run(lambda: self.robot_drive.drive_slow(
                 self.driver_controller_raw.get_axis("LY", 0.06) * DriveConstants.kMaxSpeed,
                 self.driver_controller_raw.get_axis("LX", 0.06) * DriveConstants.kMaxSpeed,
@@ -132,17 +129,17 @@ class RobotContainer:
             commands2.cmd.run(lambda: self.robot_drive.auto_balance(-1), [self.robot_drive]))
 
         # When RT is pressed, put the arm and intake to their setpoints for intaking.
-        commands2.Trigger(lambda: self.driver_controller_raw.get_trigger("R", 0.3)).whileTrue(
+        commands2.Trigger(lambda: self.operator_controller_raw.get_trigger("R", 0.3)).whileTrue(
             commands2.ParallelCommandGroup(
                 commands2.cmd.run(lambda: self.arm.set_setpoint("intake"), [self.arm]),
                 commands2.cmd.run(lambda: self.intake.intake(False, 0.9), [self.intake])))
 
-        # When A is pressed, the intake spits out anything it has in it without moving the arm
-        commands2.Trigger(lambda: self.driver_controller_raw.get_button("A")).whileTrue(
+        # When LB is pressed, the intake spits out anything it has in it without moving the arm
+        commands2.Trigger(lambda: self.operator_controller_raw.get_button("LB")).whileTrue(
             commands2.cmd.run(lambda: self.intake.intake(True, 0.5), [self.intake]))
 
         # When left trigger is pressed, fire at the speed matched to the arm setpoint
-        commands2.Trigger(lambda: self.driver_controller_raw.get_trigger("L", 0.5)).whileTrue(
+        commands2.Trigger(lambda: self.operator_controller_raw.get_trigger("L", 0.5)).whileTrue(
             commands2.cmd.run(lambda: self.intake.bound_shoot(self.arm), [self.intake, self.arm]))
 
         # Pressing "MENU" enables the directional estimation controller.
@@ -166,10 +163,28 @@ class RobotContainer:
             commands2.cmd.run(lambda: self.leds.flash_color([255, 0, 0], 5), [self.leds]))
 
         # Force arm to stow.
-        commands2.Trigger(lambda: self.driver_controller_raw.get_button("X")).toggleOnTrue(
+        commands2.Trigger(lambda: self.operator_controller_raw.get_button("RB")).toggleOnTrue(
             commands2.ParallelCommandGroup(
                 commands2.cmd.run(lambda: self.arm.set_setpoint("stow"), [self.arm]),
                 commands2.cmd.run(lambda: self.intake.intake(False, 0), [self.intake])))
+
+        # Use the operator controller to set arm setpoints.
+        commands2.Trigger(lambda: self.operator_controller_raw.get_d_pad_pull("N")).toggleOnTrue(
+            commands2.cmd.run(lambda: self.arm.set_setpoint("shoot_high_back"), [self.arm]))
+        commands2.Trigger(lambda: self.operator_controller_raw.get_d_pad_pull("E")).toggleOnTrue(
+            commands2.cmd.run(lambda: self.arm.set_setpoint("shoot_mid_front"), [self.arm]))
+        commands2.Trigger(lambda: self.operator_controller_raw.get_d_pad_pull("W")).toggleOnTrue(
+            commands2.cmd.run(lambda: self.arm.set_setpoint("shoot_mid_back"), [self.arm]))
+        commands2.Trigger(lambda: self.operator_controller_raw.get_d_pad_pull("S")).toggleOnTrue(
+            commands2.cmd.run(lambda: self.arm.set_setpoint("shoot_high_front"), [self.arm]))
+
+        # Configure manual mode for the arm and intake.
+        commands2.Trigger(lambda: self.operator_controller_raw.get_button("A")).toggleOnTrue(
+            commands2.cmd.run(lambda: self.arm.manual_control(
+                self.operator_controller_raw.get_axis("LY", 0.05)), [self.arm]))
+        commands2.Trigger(lambda: self.operator_controller_raw.get_button("A")).toggleOnTrue(
+            commands2.cmd.run(lambda: self.intake.manual_control(
+                self.operator_controller_raw.get_axis("RY", 0.05)), [self.intake]))
 
     def getAutonomousCommand(self) -> commands2.cmd:
         """Use this to pass the autonomous command to the main Robot class.
