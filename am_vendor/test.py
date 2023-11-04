d0f46e14f9078ecf889917292477a553423e91c4
Ethan Scime <44032434+escime@users.noreply.github.com>
2023-11-04 14:03:09 -0400

    Clear test.py

--- am_vendor/test.py
+++ am_vendor/test.py
@@ -1,16 +1,1 @@
-from wpilib import CAN  # CANData, CANStatus
-
-
-class AMHBE:
-    """TEST"""
-    can_id = None
-
-    def __init__(self, can_id):
-        super().__init__()
-        self.can_id = can_id
-
-    def set_absolute(self):
-        CAN.writePacket(self.can_id, 1, 1234)
-
-    def set_relative(self):
-        CAN.writePacket(self.can_id, 0, 1234)
+# NULL
